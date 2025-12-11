ab36a5ea1e0474b4512ce89c885068b0ef7bb65c
İbrahim Kaan Bilir <125301699+abes400@users.noreply.github.com>
2025-12-11 06:55:42 +0300

    Update game.cpp

--- project/src/game.cpp
+++ project/src/game.cpp
@@ -8,21 +8,35 @@
 #include <iostream>
 using namespace std;
 
+#define LOCK_HEIGHT       325
+#define LOCK_FRAME_COUNT  7
+#define LOCK_FRAME_TIME   0.05
+
+#define ARROW_HEIGHT      100
+#define ARROW_FRAME_COUNT 7
+#define ARROW_FRAME_TIME  0.05
+
 void Game() {
-    // Init game vars
-    short tileCount = Globals::difficulty + 3;
-    
-    cout << "The difficulty is " << Globals::difficulty << " so the tile count will be " << tileCount << endl;
+    ////////////////////
+    // Init game vars //
+    ////////////////////
 
     // Initialize Lock Animation Sprite
-    static const AnimatedSpriteProp lockProp = AnimatedSpriteProp {
-        &Assets::lockwindow, 325, 7, 0.05
-    };
-
-    AnimatedSprite* lockAnim = new AnimatedSprite(Vector2{CENTER_X_WINDOW, CENTER_Y_WINDOW - 100}, &lockProp, LOOPABLE);
+    static const AnimatedSpriteProp lockProp = AnimatedSpriteProp { &Assets::lockwindow, LOCK_HEIGHT, LOCK_FRAME_COUNT, LOCK_FRAME_TIME };
+    AnimatedSprite* lockAnim                 = new AnimatedSprite ( Vector2{CENTER_X_WINDOW, CENTER_Y_WINDOW - 100}, &lockProp, LOOPABLE );
 
     // Initialize arrow tiles
+    const  short tileCount = Globals::difficulty + 3;
+    static const AnimatedSpriteProp tileProp = AnimatedSpriteProp { &Assets::arrowtile, ARROW_HEIGHT, ARROW_FRAME_COUNT, ARROW_FRAME_TIME };
+    float tileX                       = CENTER_X_WINDOW - Assets::arrowtile.width * (tileCount - 1) / 2;
+    float tileY                       = CENTER_Y_WINDOW + 100;
+    AnimatedSprite* tiles[tileCount];
+    for(short i = 0; i < tileCount; i++) {
+        tiles[i] = new AnimatedSprite(Vector2 {tileX, tileY}, &tileProp);
+        tileX += Assets::arrowtile.width;
+    }
 
+    short tile_i;
 
     // Game Loop
     while (Globals::scene == Globals::IN_GAME && !WindowShouldClose()) {
@@ -32,16 +46,19 @@ void Game() {
         // Update states
         lockAnim -> updateFrame();
         
+        
 
 
         // Draw Elements
         BeginDrawing();
         tileBG();
         lockAnim -> draw();
+        for(tile_i = 0; tile_i < tileCount; tile_i++) tiles[tile_i] -> draw();
         EndDrawing();
     }
 
     // De-init game vars in heap
+    for(tile_i = 0; tile_i < tileCount; tile_i++) delete tiles[tile_i];
     delete lockAnim;
     
 }
