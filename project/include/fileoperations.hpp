68e4d74338b94ff8721a85580d63aa0598db4ef5
abes400 <125301699+abes400@users.noreply.github.com>
2025-12-28 20:03:35 +0300

    ADD file saving logic

--- project/include/fileoperations.hpp
+++ project/include/fileoperations.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
 extern char savePath[];
+
+/**
+ * Generates the correct Save Path using the OS's API.
+ * The path is saved on a global buffer.
+ */
 int initSavePath();
+
+
 void load();
+
+/**
+ * Saves the highscores and the settings on the known path provided by the OS.
+ */
 void save();
--- project/src/fileoperations.cpp
+++ project/src/fileoperations.cpp
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <climits>
+#include "globals.hpp"
 #include "raylib.h"
 
 char savePath[PATH_MAX];
+const short saveFileBufferSize = 8 * sizeof(uint16_t) + 3 * sizeof(uint8_t);
 
 #if defined(__APPLE__)
 
 #include <sysdir.h>
 #include <glob.h>
 
+/**
+ * Generates the correct Save Path using the OS's API.
+ * The path is saved on a global buffer.
+ */
 int initSavePath() {
 
     char path[PATH_MAX];
@@ -34,7 +41,10 @@ int initSavePath() {
 }
 
 #elif defined(_WIN32)
-
+/**
+ * Generates the correct Save Path using the OS's API.
+ * The path is saved on a global buffer.
+ */
 int initSavePath() {
     
 }
@@ -45,8 +55,39 @@ void load() {
     
 }
 
+/**
+ * Saves the highscores and the settings on the known path provided by the OS.
+ */
 void save() {
+    uint8_t     saveFileBuffer[saveFileBufferSize];                 // Buffer to be written from
+    uint8_t*    cursor      = saveFileBuffer + sizeof(uint16_t);    // Skipped by uint16_t size for the first checksum!
+    uint8_t     tempOpts[]  = {Opts::msc, Opts::sfx, Opts::fsc};    // Used separate array for sake of performance
+    uint16_t    checksum    = Opts::msc + Opts::sfx + Opts::fsc;    // The validation value written on both ends of the buffer
+                                                                    // Initialized w/ values of settings
 
-    SaveFileText(savePath, "testo");
 
+    // Copying timed mode highscores on buffer (and computing checksum)
+    memmove(cursor, Globals::highscores, 3 * sizeof(uint16_t));
+    checksum += Globals::highscores[Globals::EASY] 
+              + Globals::highscores[Globals::MEDIUM] 
+              + Globals::highscores[Globals::HARD];
+    cursor   += 3 * sizeof(uint16_t);
+
+    // Copying endless mode highscores on buffer (and computing checksum)
+    memmove(cursor, Globals::highscoresEndless, 3 * sizeof(uint16_t));
+    checksum += Globals::highscoresEndless[Globals::EASY] 
+              + Globals::highscoresEndless[Globals::MEDIUM] 
+              + Globals::highscoresEndless[Globals::HARD];
+    cursor   += 3 * sizeof(uint16_t);
+
+    // Copying settings on buffer (checksum already computed)
+    memmove(cursor, tempOpts, 3 * sizeof(uint8_t));
+    cursor += 3 * sizeof(uint8_t);
+
+    // Copying checksums on each end of the buffer
+    memmove(cursor, &checksum, sizeof(uint16_t));
+    memmove(saveFileBuffer, &checksum, sizeof(uint16_t));
+
+    // Saving the buffer contents on the file
+    SaveFileData(savePath, saveFileBuffer, saveFileBufferSize);
 }
